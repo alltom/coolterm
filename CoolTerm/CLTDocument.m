0f3da88d2b272c81e0998d77be60a875095bdda9
Tom Lieber <tom@alltom.com>
2014-01-19 00:19:29 -0500

    removed some unused code

--- CoolTerm/CLTDocument.m
+++ CoolTerm/CLTDocument.m
@@ -10,26 +10,9 @@
 
 @implementation CLTDocument
 
-- (id)init
-{
-    self = [super init];
-    if (self) {
-        // Add your subclass-specific initialization here.
-    }
-    return self;
-}
-
 - (NSString *)windowNibName
 {
-    // Override returning the nib file name of the document
-    // If you need to use a subclass of NSWindowController or if your document supports multiple NSWindowControllers, you should remove this method and override -makeWindowControllers instead.
     return @"CLTDocument";
-}
-
-- (void)windowControllerDidLoadNib:(NSWindowController *)aController
-{
-    [super windowControllerDidLoadNib:aController];
-    // Add any code here that needs to be executed once the windowController has loaded the document's window.
 }
 
 + (BOOL)autosavesInPlace
